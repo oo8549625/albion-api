66cff681f2b807e5a2d767366519627f0996a2cb
yeliwei <a0970366492@gmail.com>
2022-11-12 16:19:58 +0800

    update

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const axios = require('axios');
 const port = process.env.PORT || 3000;
 const redis = require('redis');
 const client = redis.createClient({
-    url: process.env.REDIS_URL || '',
+    url: process.env.REDIS_URL || 'redis://default:redispw@localhost:49153',
     password: process.env.REDIS_PASSWORD || ''
 });
 const expires = 24 * 60 * 60 * 7;
 const defaultLevel = '4.0,4.1,4.2,5.0,5.1,5.2,6.0,6.1';
+const defaultLocation = 'Thetford';
 const albion_url = 'https://www.albion-online-data.com';
 
 app.use(express.static('public'));
@@ -18,7 +19,7 @@ app.get('/', (req, res) => {
     res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
 app.get('/api/prices/resource/:item', async (req, res) => {
-    let { level, location } = req.query || ''
+    let { level, location } = req.query
     let itemsLevelList = level ? level.split(',') : defaultLevel.split(',')
     let itemPricesList = []
     if (await client.exists(req.originalUrl)) {
@@ -32,7 +33,7 @@ app.get('/api/prices/resource/:item', async (req, res) => {
             let name = enchantment > 0 ? `T${levelTag}_${req.params.item}_LEVEL${enchantment}@${enchantment}` : `T${levelTag}_${req.params.item}`
             let url = `${albion_url}/api/v2/stats/Prices/${name}.json`
             console.log('獲取資源URL: ' + url) //debug
-            resolvedPromisesArray.push(axios.get(url, { params: { locations: location } }));
+            resolvedPromisesArray.push(axios.get(url, { params: { locations: location || defaultLocation } }));
         }
         console.log('------------------------------獲取資源URL分隔線------------------------------') //debug
         try {
@@ -54,7 +55,7 @@ app.get('/api/prices/resource/:item', async (req, res) => {
 });
 
 app.get('/api/prices/equip/:item', async (req, res) => {
-    let { level, location } = req.query || ''
+    let { level, location } = req.query
     let itemsLevelList = level ? level.split(',') : defaultLevel.split(',')
     let itemPricesList = []
     if (await client.exists(req.originalUrl)) {
@@ -68,7 +69,7 @@ app.get('/api/prices/equip/:item', async (req, res) => {
             let name = enchantment > 0 ? `T${levelTag}_${req.params.item}@${enchantment}` : `T${levelTag}_${req.params.item}`
             let url = `${albion_url}/api/v2/stats/Prices/${name}.json`
             console.log('獲取裝備URL: ' + url) //debug
-            resolvedPromisesArray.push(axios.get(url, { params: { locations: location } }))
+            resolvedPromisesArray.push(axios.get(url, { params: { locations: location || defaultLocation } }))
         }
         console.log('------------------------------獲取裝備URL分隔線------------------------------') //debug
         try {
@@ -99,7 +100,7 @@ app.get('/api/prices/equip/:item', async (req, res) => {
 });
 
 app.get('/api/prices/artifact/:item', async (req, res) => {
-    let { level, location } = req.query || ''
+    let { level, location } = req.query
     let itemsLevelList = level ? level.split(',') : defaultLevel.split(',')
     let itemPricesList = []
     if (await client.exists(req.originalUrl)) {
@@ -112,7 +113,7 @@ app.get('/api/prices/artifact/:item', async (req, res) => {
             let name = `T${levelTag}_${req.params.item}`
             let url = `${albion_url}/api/v2/stats/Prices/${name}.json`
             console.log('獲取神器URL: ' + url) //debug
-            resolvedPromisesArray.push(axios.get(url, { params: { locations: location } }))
+            resolvedPromisesArray.push(axios.get(url, { params: { locations: location || defaultLocation } }))
         }
         console.log('------------------------------獲取神器URL分隔線------------------------------') //debug
         try {
