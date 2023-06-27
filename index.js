f96fd8881e2e3dedd5df63b7ec013c1796595028
yeliwei <a0970366492@gmail.com>
2023-06-27 22:27:37 +0800

    remove 6階段

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = redis.createClient({
 });
 client.on("error", (err) => console.error("redis client err", err));
 const expires = 24 * 60 * 60 * 7;
-const defaultLevel = '4.0,4.1,4.2,5.0,5.1,5.2,6.0,6.1';
+const defaultLevel = '4.0,4.1,4.2,5.0,5.1,5.2';
 const defaultLocation = 'Bridgewatch';
 const albion_url = 'https://east.albion-online-data.com';
 (async () => {
